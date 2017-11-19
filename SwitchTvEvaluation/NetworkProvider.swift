d4441cf8282b324ae4c87f3a993c12fc31cac744
Aaron Bonham <aaron.bonham@mobilemonastery.com>
2017-11-20 08:00:27 +1100

    Add basic network provider

---
+++ SwitchTvEvaluation/NetworkProvider.swift
@@ -0,0 +1,30 @@
+//
+//  NetworkProvider.swift
+//  SwitchTvEvaluation
+//
+//  Created by Aaron Bonham on 20/11/17.
+//  Copyright © 2017 Aaron Bonham. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+extension Notification.Name {
+    static let feedRefreshSuccessful = Notification.Name("feedRefreshSuccessful")
+    static let requestFeedRefresh = Notification.Name("requestFeedRefresh")
+    static let feedRefreshFailed = Notification.Name("feedRefreshFailed")
+}
+
+class NetworkProvider {
+    let apiUrl = "https://pastebin.com/raw/8LiEHfwU"
+    
+    public func refreshFeed() {
+        Alamofire.request(apiUrl).response { response in
+            guard let data = response.data else {
+                NotificationCenter.default.post(name: .feedRefreshFailed, object: response.error)
+                return
+            }
+            NotificationCenter.default.post(name: .feedRefreshSuccessful, object: data)
+        }
+    }
+}
